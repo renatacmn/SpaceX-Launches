ab4b6a4b0771516742fee6ac165a41d53780e05f
Renata Cristina <cmn.renata@gmail.com>
2019-09-09 17:56:50 +0200

    Update gradle to latest version (2.24)

--- app/src/main/java/br/com/spacexlaunches/base/BaseActivity.kt
+++ app/src/main/java/br/com/spacexlaunches/base/BaseActivity.kt
@@ -2,20 +2,18 @@ package br.com.spacexlaunches.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-abstract class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
+abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector {
 
     @Inject
-    lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
-        fragmentDispatchingAndroidInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
     // Lifecycle methods
 
