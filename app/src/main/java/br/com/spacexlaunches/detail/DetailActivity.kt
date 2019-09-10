537aaab62bc0b5f817311be3f79dff5673d279b2
Renata Cristina <cmn.renata@gmail.com>
2019-09-10 21:09:21 +0200

    Turn fragments into activities

--- app/src/main/java/br/com/spacexlaunches/detail/DetailActivity.kt
+++ app/src/main/java/br/com/spacexlaunches/detail/DetailActivity.kt
@@ -1,4 +1,50 @@
 package br.com.spacexlaunches.detail
 
-class DetailActivity {
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import br.com.spacexlaunches.R
+import br.com.spacexlaunches.base.BaseActivity
+import br.com.spacexlaunches.base.api.models.Launch
+import br.com.spacexlaunches.util.setFormattedDateOnTextView
+import br.com.spacexlaunches.util.setLaunchStatusOnTextView
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : BaseActivity() {
+
+    private var launch: Launch? = null
+
+    // Lifecycle methods
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        getExtras()
+        initializeViewComponents()
+    }
+
+    // Private methods
+
+    private fun getExtras() {
+        launch = intent.extras?.getParcelable(PARAM_LAUNCH)
+    }
+
+    private fun initializeViewComponents() {
+        initializeLaunchInfo()
+    }
+
+    private fun initializeLaunchInfo() {
+        launch?.setLaunchStatusOnTextView(detailsTextStatus)
+        detailsTextName.text = launch?.missionName
+        launch?.setFormattedDateOnTextView(detailsTextDate)
+    }
+
+    companion object {
+        fun start(context: Context, launch: Launch) {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(PARAM_LAUNCH, launch)
+            context.startActivity(intent)
+        }
+    }
+
 }
--- app/src/main/java/br/com/spacexlaunches/list/ListActivity.kt
+++ app/src/main/java/br/com/spacexlaunches/list/ListActivity.kt
@@ -1,4 +1,127 @@
 package br.com.spacexlaunches.list
 
-class ListActivity {
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import br.com.spacexlaunches.R
+import br.com.spacexlaunches.base.BaseActivity
+import br.com.spacexlaunches.base.api.models.Launch
+import br.com.spacexlaunches.detail.DetailActivity
+import br.com.spacexlaunches.util.ImageLoader
+import kotlinx.android.synthetic.main.activity_list.*
+import kotlinx.android.synthetic.main.activity_list_state_data.*
+import javax.inject.Inject
+
+class ListActivity : BaseActivity(), ListAdapter.Listener {
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    @Inject
+    lateinit var viewModelFactory: ListViewModelFactory
+
+    private val viewModel: ListViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(ListViewModel::class.java)
+    }
+
+    private var adapter: ListAdapter? = null
+
+    // Lifecycle methods
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        initializeViewComponents()
+        observeListViewState()
+        viewModel.getAllLaunches()
+    }
+
+    // ListAdapter.Listener override
+
+    override fun onLaunchClicked(launch: Launch) {
+        DetailActivity.start(this, launch)
+    }
+
+    // Private methods
+
+    private fun initializeViewComponents() {
+        initializeList()
+        initializeSwipeToRefreshLayout()
+        initializeBtnNewContent()
+    }
+
+    private fun initializeList() {
+        if (adapter == null) {
+            adapter = ListAdapter(this, imageLoader)
+            listLaunches.adapter = adapter
+        }
+    }
+
+    private fun initializeSwipeToRefreshLayout() {
+        listSwipeToRefresh.setOnRefreshListener {
+            viewModel.getAllLaunches()
+        }
+    }
+
+    private fun initializeBtnNewContent() {
+        listBtnNewContent.setOnClickListener {
+            adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun observeListViewState() {
+        viewModel.getListViewState().observe(this, Observer(this::updateUi))
+    }
+
+    private fun updateUi(viewState: ListViewState) {
+        when (viewState) {
+            is ListViewState.FirstTimeLoading -> showFirstTimeLoading()
+            is ListViewState.DefaultLoading -> showDefaultLoading()
+            is ListViewState.Error -> showError()
+            is ListViewState.Empty -> showEmptyState()
+            is ListViewState.UpdateList -> updateList(viewState.launches)
+            is ListViewState.AppendToList -> appendToList(viewState.newLaunchesToAppend)
+        }
+    }
+
+    private fun showFirstTimeLoading() {
+        listSwipeToRefresh.isRefreshing = false
+        layoutListStateLoading.visibility = View.VISIBLE
+    }
+
+    private fun showDefaultLoading() {
+        listSwipeToRefresh.isRefreshing = true
+        layoutListStateLoading.visibility = View.GONE
+    }
+
+    private fun showError() {
+        hideLoadings()
+        showToast(getString(R.string.launch_list_msg_error_state))
+    }
+
+    private fun showEmptyState() {
+        hideLoadings()
+        layoutListStateEmpty.visibility = View.VISIBLE
+        layoutListStateData.visibility = View.GONE
+    }
+
+    private fun updateList(launches: List<Launch>) {
+        hideLoadings()
+        listBtnNewContent.visibility = View.GONE
+        layoutListStateEmpty.visibility = View.GONE
+        layoutListStateData.visibility = View.VISIBLE
+        adapter?.updateAdapter(launches)
+    }
+
+    private fun appendToList(newLaunchesToAppend: List<Launch>) {
+        listBtnNewContent.visibility = View.VISIBLE
+        adapter?.appendToList(newLaunchesToAppend)
+    }
+
+    private fun hideLoadings() {
+        listSwipeToRefresh.isRefreshing = false
+        layoutListStateLoading.visibility = View.GONE
+    }
+
 }
